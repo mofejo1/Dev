e8cfe27c959b21f56b8f63e73a05246ee0598683
mofejo1 <eyinimofe98@gmail.com>
2026-05-12 22:44:15 +0100

    new

---
+++ 1_JoeAbah_UdemyCourse/mine/6section/1composition/main.go
@@ -0,0 +1,3 @@
+package main
+
+//composition is a design principle in which a class is composed of one or more objects of other classes, rather than inheriting from them. This allows for greater flexibility and modularity in the design of the class, as it can use the functionality of the composed objects without being tightly coupled to their implementation. In Go, composition is often achieved through the use of struct embedding, where one struct can embed another struct to gain access to its fields and methods.
